bce88d71c6d56e664f928b9ce6a944c010c496d6
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-11-30 23:37:45 +0700

    7. Re-building the App with Vue

--- Section 1/gs-01-starting-project/app.js
+++ Section 1/gs-01-starting-project/app.js
@@ -1,14 +1,31 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data: function() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
 
-function addGoal(){
-  const enteredValue = inputEl.value;
-  // create DOM list to store enteredValue
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  // clear text input
-  inputEl.value='';
-}
-buttonEl.addEventListener('click',addGoal);
+}).mount('#app');
+
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal(){
+//   const enteredValue = inputEl.value;
+//   // create DOM list to store enteredValue
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   // clear text input
+//   inputEl.value='';
+// }
+// buttonEl.addEventListener('click',addGoal);
