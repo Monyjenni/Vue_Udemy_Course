b54e50445b705af75cd8bbcf598973848f53b96c
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-12-16 23:41:10 +0700

    testing

--- Section 1/gs-01-starting-project/app.js
+++ Section 1/gs-01-starting-project/app.js
@@ -15,17 +15,17 @@ Vue.createApp({
 }).mount('#app');
 
 
-// const buttonEl = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
 
-// function addGoal(){
-//   const enteredValue = inputEl.value;
-//   // create DOM list to store enteredValue
-//   const listItemEl = document.createElement('li');
-//   listItemEl.textContent = enteredValue;
-//   listEl.appendChild(listItemEl);
-//   // clear text input
-//   inputEl.value='';
-// }
-// buttonEl.addEventListener('click',addGoal);
+function addGoal(){
+  const enteredValue = inputEl.value;
+  // create DOM list to store enteredValue
+  const listItemEl = document.createElement('li');
+  listItemEl.textContent = enteredValue;
+  listEl.appendChild(listItemEl);
+  // clear text input
+  inputEl.value='';
+}
+buttonEl.addEventListener('click',addGoal);
