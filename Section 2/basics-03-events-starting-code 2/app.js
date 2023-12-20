3cda5fc594fca53a85d94712666ba0d5167f41fb
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-12-20 14:28:48 +0700

    25. Exploring Event Modifiers

--- Section 2/basics-03-events-starting-code 2/app.js
+++ Section 2/basics-03-events-starting-code 2/app.js
@@ -2,20 +2,29 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
+      confirmedName: "",
     };
   },
   methods: {
+    confirmedInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      // event.preventDefault();
+      console.log("It is working !");
+      window.alert("Form is submitted ! ");
+    },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
     },
-    // input from the event 
-    setName(event,lastName) {
-      this.name = event.target.value + ' ' + lastName;
-    }
+    // input from the event
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
   },
 });
 
