d2577465c83ceb20b63718e18b7a74abaea84452
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-12-23 01:18:36 +0700

    32. v-bind and v-on Shorthands

--- Section 2/basics-03-events-starting-code 2/app.js
+++ Section 2/basics-03-events-starting-code 2/app.js
@@ -11,7 +11,10 @@ const app = Vue.createApp({
   watch: {
     counter(value){
       if(value > 50){
-        this.counter = 0;
+        const that = this
+        setTimeout(function() {
+          that.counter = 0;
+        },2000);
       }
     }
     // when name changes will be triggered
