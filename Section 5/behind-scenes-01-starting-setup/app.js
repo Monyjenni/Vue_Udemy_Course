fe4c0c54ce893f3ec8a7efae636f7203032d944f
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-12-28 21:52:33 +0700

    61. Vue Reactivity: A Deep Dive

--- Section 5/behind-scenes-01-starting-setup/app.js
+++ Section 5/behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,35 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!'
+}
+
+const handler = {
+  set(target, key, value) {
+    // console.log(target,key,value)
+    if(key === 'message') {
+      target.longMessage = value + 'World'
+    }
+    target.message = value;
+  }
+}
+//proxy wrap data msg
+const proxy = new Proxy(data,handler );
+// just wanna trigger when message is changed from the previous
+proxy.message = 'Hello!!!'
+console.log('new one', proxy.longMessage)
+
+// let message = 'Hello';
+
+
+// let longMessage = message + 'Monie';
+
+// console.log(longMessage);
+
+// message= 'Hello!!!'
+
+// console.log(longMessage)
+
