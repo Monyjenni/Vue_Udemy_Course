2920b969fc7fe887b2822914612165dfaf072070
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-02 13:17:53 +0700

    71.Introducing Components

--- Section 6/cmp-intro-01-starting-setup/app.js
+++ Section 6/cmp-intro-01-starting-setup/app.js
@@ -1,27 +1,45 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
-          id: 'mony yoeun',
-          name: 'mony yoeun',
-          phone: '01234567',
-          email: 'mony@gmail.com'
+          id: "jen yoeun",
+          name: "jen yoeun",
+          phone: "01234567",
+          email: "jen@gmail.com",
         },
-        {
-          id: 'jen yoeun',
-          name: 'jen yoeun',
-          phone: '01234567',
-          email: 'jen@gmail.com'
-        }
-      ]
-    }
+      ],
+    };
+  },
+});
+//always contain dash to make up multi words
+app.component("friend-contact", {
+  template: `
+  <li>
+  <h2>{{ friend.name }}</h2>
+  <button @click="toggleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details </button>
+  <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong> {{ friend.phone }}</li>
+    <li><strong>Email:</strong> {{ friend.email }}</li>
+  </ul>
+</li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: 
+      {
+        id: "mony yoeun",
+        name: "mony yoeun",
+        phone: "01234567",
+        email: "mony@gmail.com",
+      },
+    };
   },
   methods: {
     toggleDetails() {
-      this.detailsAreVisible =! this.detailsAreVisible
-    }
-  }
-})
-app.mount('#app')
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+app.mount("#app");
