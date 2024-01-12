2d228ddffc38608836e0f4518904ebbc732fb3cd
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-12 16:32:46 +0700

    86. Creating a Basic Vue App

---
+++ Section 7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h2>My Friends </h2>
+    <ul>
+      <li>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        friends: [
+          {
+            id: 'Mony',
+            name: 'Sovan Mony',
+            phone: '12345678',
+            email: 'chansovanmony@gmail.com'
+          },
+          {
+            id: 'Jen',
+            name: 'Jennie',
+            phone: '12345678',
+            email: 'jen@gmail.com'
+          }
+        ]
+      }
+    }
+  }
+
+</script>
+
+
+<style scoped>
+
+</style>
