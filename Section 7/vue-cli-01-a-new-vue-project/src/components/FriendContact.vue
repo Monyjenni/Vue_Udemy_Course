01aaa0c425ca368a7adc67fcd54aea09c0a0e0e0
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-12 16:54:48 +0700

    87. Adding a Component

---
+++ Section 7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>
+      {{ friend.name }}
+      <button @click="toggleDetails">Show Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </h2>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'Mony',
+        name: 'Sovan Mony',
+        phone: '12345678',
+        email: 'chansovanmony@gmail.com'
+      },
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    }
+  }
+}
+</script>
+
+<style scoped></style>
