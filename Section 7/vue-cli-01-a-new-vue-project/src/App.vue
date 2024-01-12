f9e6c98782b03904815d27ce22680dbc8c75b17f
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-12 16:58:53 +0700

    88. Adding Styling

--- Section 7/vue-cli-01-a-new-vue-project/src/App.vue
+++ Section 7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
-    <h2>My Friends </h2>
+    <header><h1>My Friends </h1></header>
     <ul>
+      <friend-contact/>
       <friend-contact/>
     </ul>
   </section>
@@ -35,5 +36,67 @@ import FriendContact from './components/FriendContact.vue'
 
 
 <style scoped>
+* {
+  box-sizing: border-box;
+}
 
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #6c015e;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid red;
+  background-color: red;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
 </style>
