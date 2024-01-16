14bbce384bd2a73a6d776271128dbb58e93ecaa0
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-16 21:51:34 +0700

    95. Validating Props

--- Section 7/vue-cli-01-a-new-vue-project/src/App.vue
+++ Section 7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -5,7 +5,7 @@
     </header>
     <ul>
       <friend-contact name="Mony" phone-number="12345" email-address="mony@gmail.com" is-favorite="1" />
-      <friend-contact name="Jen" phone-number="1234567" email-address="jen@gmail.com" is-favorite="0"/>
+      <friend-contact name="Jen" phone-number="1234567" email-address="jen@gmail.com"/>
     </ul>
   </section>
 </template>
