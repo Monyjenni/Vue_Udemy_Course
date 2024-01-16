0c95d7e00e240531183dfbe5255cf4d0da26ab59
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-16 23:26:58 +0700

    101. Demo: Adding Components & Connecting Them

--- Section 7/vue-cli-01-a-new-vue-project/src/App.vue
+++ Section 7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <section>
     <header>
-      <h1>My Friends </h1>
+      <h2>My Friends </h2>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact v-for="friend in friends" :key="friend.id"  :id="friend.id" :name="friend.name" :phone-number="friend.phone" :email-address="friend.email" :is-favorite="friend.isFavorite" @toggle-favorite="toggleFavoriteStatus" />
     </ul>
@@ -11,8 +12,9 @@
 
 <script>
 import FriendContact from './components/FriendContact.vue'
+import NewFriend from './components/NewFriend.vue'
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
       friends: [
@@ -38,7 +40,19 @@ export default {
       const identifiedFriend = this.friends.find(friend => friend.id === friendId)
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite
       
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false
+      }
+      this.friends.push(newFriendContact)
+
     }
+
   }
 }
 
@@ -78,7 +92,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -110,5 +125,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
