f4aa660551325d45d2be17bd84dfb831b2c6642d
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-17 11:35:28 +0700

    103. A Potential Problem

---
+++ Section 7/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <!-- pass through , pass knw g to topic then  -->
+    <knowledge-grid :topics="topics" @select-topic="$emit('select-topic', $event)"></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['topics'],
+  emits: ['select-topic'],
+};
+</script>
