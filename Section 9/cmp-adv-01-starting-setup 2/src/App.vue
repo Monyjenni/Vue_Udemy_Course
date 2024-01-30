951a4a113c01c91a6fb43dbe985ee7604f3e7db3
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-01-30 11:11:06 +0700

    117. Scoped Slots

--- Section 9/cmp-adv-01-starting-setup 2/src/App.vue
+++ Section 9/cmp-adv-01-starting-setup 2/src/App.vue
@@ -7,28 +7,36 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps["another-prop"] }}</p></template
+      >
+    </course-goals>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoal.vue";
 export default {
   data() {
     return {
       activeUser: {
-        name: 'Chansovanmony Yoeun',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Chansovanmony Yoeun",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
   components: {
-    'the-header':TheHeader,
+    "the-header": TheHeader,
     BadgeList,
-    UserInfo
-  }
+    UserInfo,
+    CourseGoals,
+  },
 };
 </script>
 
