d44d1ff65b98762856d411583adbbccc2c6c3102
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-08-08 10:59:09 +0700

    227. Structuring Vuex Code & Files

--- Section 15/vuex-01-starting-setup/src/components/store/index.js
+++ Section 15/vuex-01-starting-setup/src/components/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'vuex';
-import rootMutations from './mutations.js'
-import rootActions from './actions.js'
+import rootMutations from './mutations.js';
+import rootActions from './actions.js';
+import rootGetters from './getters.js';
+import counterModule from './modules/counter/index.js';
 const store = createStore({
   modules: { 
     numbers: counterModule
@@ -15,11 +17,7 @@ const store = createStore({
   //caller
   actions: rootActions,
   //getter?
-  getters: {
-    userIsAuthenticated(state) {
-      return state.isLoggedIn
-    }
-  },
+  getters: rootGetters ,
 })
 
 export default store;
