a3c10a78aed33b065512e00224adf96744d63a76
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-08-16 11:41:44 +0700

    231. Challenge Solution (3/3)

--- Section 15/vuex-11-a-challenge-starting-code/src/store/index.js
+++ Section 15/vuex-11-a-challenge-starting-code/src/store/index.js
@@ -6,6 +6,34 @@ const store = createStore({
   modules: {
     prods: productsModule,
     cart: cartModule
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+    }
+  },
+  mutations: {
+    methods: {
+      login() {
+        state.isLoggedIn = true;
+      },
+      logout() {
+        state.isLoggedIn = false;
+      },
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit('login')
+    },
+    logout(context) {
+      context.commit('logout')
+    }
+  },
+  getters: {
+    isAuthenticated (state) {
+      return state.isLoggedIn;
+    }
   }
 });
 
