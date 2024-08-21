14fcb6e79a4077b1d9184aa2ad1b2206de7d0875
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-08-21 10:46:11 +0700

    241. Wiring Up Pages

--- Section 16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ Section 16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -4,6 +4,10 @@
     FILTER
   </section>
   <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
     LIST OF COACHES
   </section>
 </div>
