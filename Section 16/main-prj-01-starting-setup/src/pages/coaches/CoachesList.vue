3494a5d6040c75ed24c81bd92813523bf0a9cdba
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-08-21 09:39:35 +0700

    240. Working on the Main Layout & Styling

---
+++ Section 16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  <section>
+    FILTER
+  </section>
+  <section>
+    LIST OF COACHES
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- Section 16/main-prj-01-starting-setup/src/router.js
+++ Section 16/main-prj-01-starting-setup/src/router.js
@@ -3,9 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/coaches/ContactCoach.vue';
-import RequestsReceived from './pages/coaches/RequestsReceived.vue';
-import NotFound from './pages/coaches/NotFound.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
