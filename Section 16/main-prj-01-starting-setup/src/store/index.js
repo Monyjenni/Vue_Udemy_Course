bc5174ff055f26c21a4b2d8530a17fe775b0abf1
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-08-22 10:30:42 +0700

    fix: module-index.js path

--- Section 16/main-prj-01-starting-setup/src/store/index.js
+++ Section 16/main-prj-01-starting-setup/src/store/index.js
@@ -1,7 +1,5 @@
 import { createStore } from 'vuex';
-
-import coachesModule from './modules/coaches/index.js'
-import coaches from '../modules/coaches/index.js';
+import coachesModule from './modules/coaches/index.js';
 
 const store = createStore({
   modules:{
